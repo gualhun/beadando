614ce0bc9f4e8862cc736265fc5caff7b4b8ad36
janos_guraly <guraly.janos89@gmail.com>
2017-04-30 12:14:34 +0200

    dao

--- cinema/src/main/java/hu/me/CinemaApplication.java
+++ cinema/src/main/java/hu/me/CinemaApplication.java
@@ -2,6 +2,8 @@ package hu.me;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
@@ -11,10 +13,15 @@ public class CinemaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaApplication.class, args);
 	}
+	
+	@Bean
+	public HibernateJpaSessionFactoryBean sessionFactory() {
+		return new HibernateJpaSessionFactoryBean();
+	}
 }
 
 //aha
-//http://localhost:8181/h2-console/
+//http://localhost:8282/h2-console/
 //postman adat felvitel:
 //{
 //	"title": "Star Trek",
